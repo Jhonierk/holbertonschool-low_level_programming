ec4c8e3b0796e49aee3d7486544dd057ee51ad3d
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-06-26 09:03:37 -0500

    0x06-7 function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,24 @@
 #include "holberton.h"
 
 /**
- * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
+ * leet - function that encodes a string into 1337.
+ * @c: take an string
+ * Return: the text encrypted
  */
-
-char *leet(char *s)
+char *leet(char *c)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	char *c1 = "aeotlAEOTL";
+	char *c2 = "4307143071";
+
+	for (i = 0; c[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; c1[j] != '\0'; j++)
+		{
+			if (c[i] == c1[j])
+				c[i] = c2[j];
+		}
 	}
-
-	return (s);
+	return (c);
 }
