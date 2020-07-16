2623e994b6ca9d175696774ea0d594c0bd02e60e
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-07-15 22:17:56 -0500

    0x0F-0 function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
-#include "function_pointers.h"
-
+#include"function_pointers.h"
+#include<stdlib.h>
+#include<stdio.h>
 /**
  * print_name - Print a name from a function pointer
  * @name: char string
