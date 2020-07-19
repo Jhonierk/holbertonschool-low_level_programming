c8d17e04cdfc200da4b288638c5e34e89ec4d663
Jhonierk <jorge.otalvaro00@usc.edu.co>
2020-07-19 19:49:06 +0000

    0x10-4 Advanced Write a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,11 +1,11 @@
 section .data
 
-    message db "Hello, Holberton!", 10
+    message db "Hello, Holberton", 10
 
 section .text
 
-global main 
-main: 
+global main
+main:
     mov rax, 1
     mov rdi, 1
     mov rsi, message
