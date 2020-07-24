77c3b488cb57609791199b0cfefc1ae326f435a7
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-07-24 14:16:30 -0500

    The prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
+#include <stdarg.h>
+#include <string.h> 
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,8 +19,9 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-
-#endif
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+#endif /* VARIADIC_FUNCTIONS_H */
